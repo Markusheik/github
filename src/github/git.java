cf333c472fa93c922636f19ca60fb35aeb72fd85
Markusheik <markus.2.heikkinen@student.hamk.fi>
2023-10-22 18:57:50 +0300

    Aikuis ikä

--- src/github/git.java
+++ src/github/git.java
@@ -2,19 +2,4 @@ package github;
 
 public class git {
 
-	public static void main(String[] args) {
-		
-		 int ika = 17;
-		    if (ika >= 0 && ika < 18)
-		    {
-		    	System.out.println("Olet alaikäinen");
-		    	if (ika >= 15)
-		    	{
-		    		System.out.println("Saat ajaa mopoa");
-		    	} if (ika >= 16)
-		    	{
-		    		System.out.println("Saat ajaa kevaria"); 
-		
-	}
-}} 
-	}
+	
---
+++ src/snippet/Snippet.java
@@ -0,0 +1,39 @@
+package snippet;
+
+public class Snippet {
+		public static void main(String[] args) {
+			
+	    int ika = 19;
+	    if (ika >= 0 && ika < 18)
+	    {
+	    	System.out.println("Olet alaikäinen");
+	    	if (ika >= 15)
+	    	{
+	    		System.out.println("Saat ajaa mopoa");
+	    	} if (ika >= 16)
+	    	{
+	    		System.out.println("Saat ajaa kevaria");    	    	
+	    }
+	    }
+	    	if (ika >= 18 && ika < 65)
+	    	{
+	    		System.out.println("Olet aikuinen");
+	    	}
+	    	if (ika > 18)
+	    	{
+	    		System.out.println("Saat ajaa autoa");
+	    	}
+	  
+			
+	    	
+	    	
+	    	
+	    	
+	    	
+	    	
+		}
+	
+	}
+	
+
+
