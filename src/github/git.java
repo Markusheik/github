d05e4c797fc9d34da3cefe865c89a01603c315a8
Markusheik <markus.2.heikkinen@student.hamk.fi>
2023-10-22 18:51:03 +0300

    18 ikä lisäys

--- src/github/git.java
+++ src/github/git.java
@@ -3,8 +3,18 @@ package github;
 public class git {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		 int ika = 17;
+		    if (ika >= 0 && ika < 18)
+		    {
+		    	System.out.println("Olet alaikäinen");
+		    	if (ika >= 15)
+		    	{
+		    		System.out.println("Saat ajaa mopoa");
+		    	} if (ika >= 16)
+		    	{
+		    		System.out.println("Saat ajaa kevaria"); 
+		
 	}
-
-}
+}} 
+	}
